106fd89922d88c38b167544f1a9bf3bdc376205d
Anuranj N <anuranj2002@gmail.com>
2022-12-23 04:52:59 +0530

    added switch statement programs

---
+++ Homework Questions/exit.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(){
+    while(1){
+        cout<<"How are you doing??"<<endl;
+
+        int num=1;
+        switch(num){
+            case 1:cout<<"Am fine"<<endl;
+            break;
+        }
+        exit(0);
+        
+    }
+}
